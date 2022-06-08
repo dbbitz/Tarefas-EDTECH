69caa4b3d45fb08417584fa428ffff0714277607
Daniel.dr206@gmail.com <Daniel.dr206@gmail.com>
2022-06-08 09:44:29 -0300

    correçao no ex

--- JS/tarefa04/ex-a/index.js
+++ JS/tarefa04/ex-a/index.js
@@ -1,11 +1,16 @@
 function calcular(){
     var peso = document.querySelector("input#peso").value
     var altura = document.querySelector("input#altura").value
-    var resultado = document.querySelector("p")
+    var resultado = document.getElementById("r1")
+    var classificacao = document.getElementById("r2")
     if (isNaN(peso) == true || isNaN(altura) == true){
         alert("Coloque apenas números")
     }
     var npeso = Number(peso)
     var naltura = Number(altura)
-    resultado.innerHTML = `Seu IMC é igual á ${npeso/(naltura*naltura)}`
+    var imc = npeso/(naltura*naltura)
+    resultado.innerHTML = `Seu IMC é igual á ${imc.toFixed(1)}`
+    if(imc<18.5){classificacao.innerHTML = "Você está abaixo do peso"}
+    if(18.5<imc && imc<24.9){classificacao.innerHTML = "Você está no seu peso ideal"}
+    if(25<imc){classificacao.innerHTML = "Você está acima do peso"}
 }
--- JS/tarefa04/ex-c/index.js
+++ JS/tarefa04/ex-c/index.js
@@ -8,18 +8,10 @@ function int(n){
 }
 function calcular(){
     var max = document.querySelector("input#max").value
-    var min = document.querySelector("input#min").value
     var resultado = document.querySelector("p")
-    if (isNaN(max) == true || isNaN(min) == true){
+    if (isNaN(max) == true){
         alert("Coloque apenas números")
     }
-    if(int(max) == false || int(min) == false){
-        alert("Coloque apenas inteiros")
-    }
     var nmax = Number(max)
-    var nmin = Number(min)
-    if(nmax<nmin){
-        alert("O máximo preciso ser maior que o minimo")
-    }
-    resultado.innerHTML = `${Math.random() * (nmax - nmin) + nmin}`
+    resultado.innerHTML = `O menor inteiro é ${Math.floor(max)} e o maior inteiro é ${Math.ceil(max)}`
 }
