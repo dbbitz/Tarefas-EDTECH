c39792da7853c7304a6989f22ab1d767901a8eb3
Daniel.dr206@gmail.com <Daniel.dr206@gmail.com>
2022-06-10 00:33:09 -0300

    ex3

--- JS/tarefa06/ex3/index.js
+++ JS/tarefa06/ex3/index.js
@@ -1,51 +1,25 @@
-function info() {
-    var data = document.getElementById('data').value.split('-')
-    var res = document.querySelector('p')    
-    var data2 = new Date(data[0], data[1], data[2])
-    var dsem = data2.getDay()
-    var mmes = data2.getMonth()
-    switch (dsem) {
-        case 1: dsem = "Sábado"
-            break
-        case 2: dsem = "Domingo"
-            break
-        case 3: dsem = "Segunda"
-            break
-        case 4: dsem = "Terça"
-            break
-        case 5: dsem = "Quarta"
-            break
-        case 6: dsem = "Quinta"
-            break
-        case 0: dsem = "Sexta"
-            break
-    }
-    switch (mmes) {
-        case 1: mmes = "Janeiro"
-            break
-        case 2: mmes = "Fevereiro"
-            break
-        case 3: mmes = "Março"
-            break
-        case 4: mmes = "Abril"
-            break
-        case 5: mmes = "Maio"
-            break
-        case 6: mmes = "Junho"
-            break
-        case 7: mmes = "Julho"
-            break
-        case 8: mmes = "Agosto"
-            break
-        case 9: mmes = "Setembro"
-            break
-        case 10: mmes = "Outubro"
-            break
-        case 11: mmes = "Novembro"
-            break
-        case 12: mmes = "Dezembro"
-            break
-    }
+function addoption(n) {
+    document.getElementById("min").add(new Option(n, n))
+    document.getElementById("seg").add(new Option(n, n))
+}
+for (i = 0; i < 60; i++) {
+        addoption(i)
+}
 
-    res.innerHTML = `Dia: ${data[2]}<br> Mês: ${data[1]} <br> Ano: ${data[0]} <br> Dia da semana:${dsem} <br> Mês por extenso: ${mmes} <br> Data em milisegundo: ${new Date(data[0],data[1],data[2]).getTime()} `
+function cont() {
+    var min = document.querySelector("select#min").value
+    var seg = document.querySelector("select#seg").value
+    var txt = document.querySelector('p')
+    var botao = document.querySelector('input')
+    botao.style.background = "red"
+    setInterval(() => {
+        seg = seg - 1
+        if(seg==-1){
+            setTimeout(1000)
+            seg = 59
+            min = min - 1
+        }
+        txt.innerHTML = `${min} : ${seg}`
+    }, 1000);
+    
 }
