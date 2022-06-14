b2cfda1c57241aef78af05ddb8c2697c2f1a924f
Daniel.dr206@gmail.com <Daniel.dr206@gmail.com>
2022-06-13 23:15:26 -0300

    adicionando ex2

--- JS/tarefa07/ex2/index.js
+++ JS/tarefa07/ex2/index.js
@@ -1,32 +1,13 @@
-function escrever() {
-    var num = document.getElementById('num').value
-    var resultado = document.querySelector('p')
-    var res = ""
-    var n = Number(num)
-    if (n < 0 || n > 10) {
-        alert("Digite um número de 1 a 10")
+var input = document.querySelector('input')
+document.addEventListener('keydown', function (event) {
+    console.log(event.key)
+    var caracteres = 0
+    if (isNaN(event.key) == true && event.key != "Backspace") {
+        alert('digite um numero')
     }
-    switch (n) {
-        case 1: res = "Um"
-            break
-        case 2: res = "Dois"
-            break
-        case 3: res = "Três"
-            break
-        case 4: res = "Quatro"
-            break
-        case 5: res = "Cinco"
-            break
-        case 6: res = "Seis"
-            break
-        case 7: res = "Sete"
-            break
-        case 8: res = "Oito"
-            break
-        case 9: res = "Nove"
-            break
-        case 10: res = "Dez"
-            break
+    
+    if(input.value.length == 5){
+        input.value += "-"
     }
-    resultado.innerHTML = res
-}
+
+})
