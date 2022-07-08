74f449aa3b8f5113a3a6385a2428c07d5097b7bb
Daniel.dr206@gmail.com <Daniel.dr206@gmail.com>
2022-07-08 00:22:05 -0300

    tarefa api

--- JS/tarefa15/index.js
+++ JS/tarefa15/index.js
@@ -1,8 +1,11 @@
-let moeda = document.querySelector("select").value
 const btn = document.querySelector("input#btn")
 const tabela = document.querySelector('table')
-function dolar(){
-    fetch('https://economia.awesomeapi.com.br/json/daily/USD-BRL/?start_date=20220701&end_date=20220701')
+const cot = document.querySelector('div#cot')
+let moeda = document.querySelector("select").value
+
+// Data no formato 'YYYYMMDD' Cash no formato 'USD' 'EUR'
+function cotacao(data, cash) {
+    fetch(`https://economia.awesomeapi.com.br/json/daily/${cash}-BRL/?start_date=20180701&end_date=${data}`)
         .then((response) => {
             return response.json()
         })
@@ -11,22 +14,50 @@ function dolar(){
             let minimo = data[0].low
             let date = data[0].create_date
             let fechamento = data[0].bid
-            tabela.innerHTML += `<td>${date}</td><td>${maximo} - ${minimo}</td> <td>${fechamento}</td>`
+            tabela.innerHTML += `<td>${date}</td><td>${minimo}${cash}</td><td>${maximo}${cash}</td><td>${fechamento}${cash}</td>`
         })
-    fetch('https://economia.awesomeapi.com.br/last/USD-BRL')
+
+    fetch(`https://economia.awesomeapi.com.br/last/${moeda}-BRL`)
         .then((response) => {
             return response.json()
         })
         .then((data) => {
             let cotacao = data.USDBRL.high
-            tabela.innerHTML += `<td>${cotacao}</td>`
+            cot.innerHTML += `<td>${cotacao}${cash}</td>`
         })
-        
 }
-function okay(){
-    return dolar()
+function addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
 }
+function format_data(data){
+    let dia = data.getDay()
+    let mes = data.getMonth()
+    let ano = data.getFullYear()
+    if(Number(mes)<10){
+        mes = `0${mes}`
+    }
+    if(Number(dia)<10){
+        dia = `0${dia}`
+    }
+    return `${ano}${mes}${dia}`
+}
+function okay() {
 
+    let datainicio = document.querySelector("input#data-inicial").value
+    datainicio = datainicio.split('-')
+    let datafim = document.querySelector("input#data-final").value
+    datafim = datafim.split('-')
+    console.log(datainicio)
+    console.log(datafim)
+
+    let datainicioUTC = new Date(datainicio[0],datainicio[1],datainicio[2])
+    let datafimUTC = new Date(datafim[0],datafim[1],datafim[2]) 
+    
+    cotacao(datainicio.join(''), 'USD')
+    
+}
 
 //AJAX
 
@@ -45,7 +76,7 @@ function okay(){
 // fetch("https://swapi.dev/api/people/1")
 //     .then(function(response){
 //         return response.json()
-        
+
 //     })
 //     .then(function (data){
 //         console.log(data)
---
+++ JS/tarefa16/index.js
@@ -0,0 +1,70 @@
+const btn = document.getElementById('btn')
+const tabela = document.querySelector('table')
+const mapa = document.querySelector('iframe')
+const cepbox = document.getElementById('cep')
+const info = []
+let resultado = document.getElementById('info')
+
+// Formatador 
+cepbox.addEventListener('keydown', function (event) {
+    console.log(cepbox.value)
+    if (cepbox.value.length == 5) {
+        cepbox.value += "-"
+    }
+    if (isNaN(event.key) == true && event.key != "Backspace" && cepbox.value.length < 9) {
+        alert('digite um numero')
+    }
+
+})
+
+
+// Evento que dispara função que lista as informações sobre o cep.
+btn.addEventListener('click', function getAddress() {
+    tabela.innerHTML = `
+        <tr>
+            <td>Rua</td>
+            <td>Bairro</td>
+            <td>Cidade</td>
+            <td>Estado</td>
+            <td>Longitude</td>
+            <td>Latitude</td>
+        </tr>
+        <tr id="info">
+        </tr>
+    `
+    let cep = document.getElementById('cep').value
+    btn.style.cursor = 'wait'
+    fetch(`https://cep.awesomeapi.com.br/json/${cep}`)
+        .then((response) => {
+            console.log(response.status)
+            if(response.status != 200){
+                alert("Cep Inválido")
+            }
+            return response.json()
+            
+        })
+        .then((data) => {
+            if(data.address == undefined){
+                mapa.style.display = 'none'
+                tabela.style.display = 'none'
+                btn.style.cursor = 'default'
+            }
+            else{
+                mapa.style.display = 'flex'
+                //Api com Parametros de Latitude e Longitude
+                mapa.src = `https://maps.google.com.br/maps?q=${data.lat},${data.lng}&output=embed&dg=oo`
+                tabela.style.display = 'flex'
+                document.getElementById('info').innerHTML += `
+                <td>${data.address}</td>
+                <td>${data.district}</td>
+                <td>${data.city}</td>
+                <td>${data.state}</td>
+                <td>${data.lng}</td>
+                <td>${data.lat}</td>
+            `
+                btn.style.cursor = 'default'
+            }
+        
+        })
+})
+
